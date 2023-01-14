9097ccc178c2833c99c8e0f0167e2e6ab14b8248
josumsc <josu93alonso@gmail.com>
2023-01-14 20:39:06 +0100

    V2 integrating tweepy

--- src/app.py
+++ src/app.py
@@ -1,5 +1,13 @@
 import streamlit as st
-from utils import check_password, load_model, get_results
+from utils import (
+    check_password,
+    load_model,
+    get_tweepy_api,
+    get_tweets_from_username,
+    get_results_df_from_tweets,
+    summarize_tweet_profiling,
+    get_text_results,
+)
 
 st.set_page_config("Sentiment Analysis App", None, "wide")
 st.write("# Sentiment Analysis App")
@@ -10,9 +18,23 @@ if check_password():
     model_name = f"cardiffnlp/twitter-roberta-base-{task}"
     tokenizer, model, labels = load_model(task, model_name)
 
-    with st.form("text-input"):
-        text = st.text_input("Enter text to classify")
-        submit_button = st.form_submit_button("Submit")
+    with st.form("username-input"):
+        username = st.text_input("Enter **@username** to classify")
+        submit_button_username = st.form_submit_button("Submit")
 
-    if submit_button:
-        get_results(text, tokenizer, model, labels)
+    if submit_button_username:
+        api = get_tweepy_api()
+        with st.spinner("Loading tweets..."):
+            tweets = get_tweets_from_username(username, api)
+        with st.spinner("Classifying tweets..."):
+            results_df = get_results_df_from_tweets(tweets, tokenizer, model)
+        summarize_tweet_profiling(username, results_df)
+
+    else:
+        st.write("Or...")
+        with st.form("text-input"):
+            text = st.text_input("Copy-paste tweet to classify")
+            submit_button_text = st.form_submit_button("Submit")
+        
+        if submit_button_text:
+            get_text_results(text, tokenizer, model, labels)
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 import streamlit as st
 from scipy.special import softmax
 import csv
@@ -6,6 +8,9 @@ import numpy as np
 import pandas as pd
 from transformers import AutoModelForSequenceClassification
 from transformers import AutoTokenizer
+import tweepy
+import plotly.express as px
+import plotly.graph_objects as go
 
 
 def check_password():
@@ -79,7 +84,7 @@ def load_model(task, model_name):
     return tokenizer, model, labels
 
 
-def get_results(text, tokenizer, model, labels):
+def get_text_results(text, tokenizer, model, labels):
     """Writes in our webapp the results obtained from the model."""
     scores = predict(text, tokenizer, model)
     ranking = np.argsort(scores)
@@ -89,3 +94,76 @@ def get_results(text, tokenizer, model, labels):
 
     result_df = pd.DataFrame(zip(labels, scores), columns=['Label', 'Score'])
     st.table(result_df.sort_values(by='Score', ascending=False).reset_index(drop=True))
+
+
+@st.experimental_singleton(show_spinner=False)
+def get_tweepy_api():
+    """Returns a tweepy API object authorized based in our .env variables."""
+    load_dotenv()
+
+    consumer_key = os.environ["API_KEY"]
+    consumer_secret = os.environ["API_KEY_SECRET"]
+    access_token = os.environ["ACCESS_TOKEN"]
+    access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+
+    auth = tweepy.OAuth1UserHandler(
+        consumer_key, 
+        consumer_secret, 
+        access_token, 
+        access_token_secret
+    )
+
+    api = tweepy.API(auth)
+    return api
+
+
+def get_tweets_from_username(username, api, count=100):
+    """Returns a list of tweets from the given username."""
+    tweets = api.user_timeline(screen_name=username, count=count, include_rts=False, tweet_mode='extended')
+    return [tweet.full_text for tweet in tweets]
+
+
+def get_results_df_from_tweets(tweets, tokenizer, model):
+    """Returns the results for the given username as DataFrame."""
+    results_df = pd.DataFrame(columns=['Tweet', 'Negative', 'Neutral', 'Positive'])
+    for tweet in tweets:
+        scores = predict(tweet, tokenizer, model)
+        prediction = np.argmax(scores)
+        results_df = results_df.append({
+            'Tweet': tweet,
+            'Negative': scores[0],
+            'Neutral': scores[1],
+            'Positive': scores[2],
+            'Sentiment': 'Negative' if prediction == 0 else 'Neutral' if prediction == 1 else 'Positive',
+        }, ignore_index=True)
+    return results_df
+
+
+def summarize_tweet_profiling(username, results_df):
+    """Returns a summary of the results for the given username."""
+    st.write(f"## Results for the tweets of **@{username}**:")
+
+    with st.container():
+        columns = st.columns(2)
+        with columns[0]:
+            st.write("### Sentiment distribution:")
+            st.plotly_chart(px.pie(results_df, names='Sentiment').update_layout(showlegend=False))
+        
+        with columns[1]:
+            st.write("### Sentiment distribution:")
+            st.plotly_chart(
+                px.box(
+                    pd.melt(results_df.drop('Tweet', axis=1)), x='variable', y='value', color='variable', points='outliers'
+                ).update_layout(showlegend=False)
+            )
+
+    with st.container():
+        st.write("## Most Sentimental Tweets:")
+        columns = st.columns(2)
+        with columns[0]:
+            st.write("### Most Negative Tweets:")
+            st.table(results_df.sort_values(by='Negative', ascending=False).head(5).reset_index(drop=True)[['Tweet', 'Negative']])
+        
+        with columns[1]:
+            st.write("### Most Positive Tweets:")
+            st.table(results_df.sort_values(by='Positive', ascending=False).head(5).reset_index(drop=True)[['Tweet', 'Positive']])
